3da641270e09ea40277023b3e5272edef062f315
Leonardo Sirino <leonardosirino@hotmail.com>
2018-04-15 18:14:00 -0300

    Desenho dos tiros

--- Code/Python/Game/Main.py
+++ Code/Python/Game/Main.py
@@ -2,10 +2,12 @@ import pygame
 import random
 import math as m
 import numpy as np
+import time
 
 # Definição de algumas constantes
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 500
@@ -23,9 +25,9 @@ game = GameSettings()
 
 class AsterManage:
     def __init__(self):
-        self.__maxAsters = 10
-        self.__minVel = 0.5
-        self.__maxVel = 3
+        self.__maxAsters = 20
+        self.__minVel = 1
+        self.__maxVel = 4
         self.maxSize = 30
         self.maxDivs = 3
         self.asters = []
@@ -69,6 +71,15 @@ class AsterManage:
         for aster in self.asters:
             aster.draw(screen)
 
+    def DeleteAster(self, ID):
+        i = 0
+        for aster in self.asters:
+            if aster.ID == ID:
+                index = i
+            i += 1
+
+        del self.asters[index]
+
 
 # Criação do gerente de asteróides
 ManagerAsters = AsterManage()
@@ -87,11 +98,90 @@ class Aster:
         self.x_pos += self.vel * m.sin(self.direction)
         self.y_pos += self.vel * m.cos(self.direction)
 
+        Cond_a = self.x_pos < -2 * \
+            ManagerAsters.maxSize or self.x_pos > (
+                SCREEN_WIDTH + 2 * ManagerAsters.maxSize)
+        Cond_b = self.y_pos < -2 * \
+            ManagerAsters.maxSize or self.y_pos > (
+                SCREEN_HEIGHT + 2 * ManagerAsters.maxSize)
+
+        if Cond_a or Cond_b:
+            ManagerAsters.DeleteAster(ID=self.ID)
+
     def draw(self, screen):
         size = ManagerAsters.maxSize / 2**(ManagerAsters.maxDivs - self.size)
-        pygame.draw.circle(screen, WHITE, (int(self.x_pos), int(self.y_pos)), int(size))
+        pygame.draw.circle(screen, WHITE, (int(self.x_pos),
+                                           int(self.y_pos)), int(size))
+        """
         print("Desenhado asteroide de ID " + str(self.ID) + " coordenadas: x = " +
               str(self.x_pos) + "; y = " + str(self.y_pos) + " -- tam: " + str(self.size))
+        """
+
+
+class ShootManage:
+    def __init__(self):
+        self.__minTimeShoots = 0.2
+        self.ShootVeloc = 20
+        self.shootSize = 4
+        self.shoots = []
+        self.lastTime = time.time() - self.__minTimeShoots
+        self.lastID = 0
+
+    def shoot(self, Nave):
+        CurrentTime = time.time()
+        if (CurrentTime - self.lastTime) > self.__minTimeShoots:
+            NewShoot = Shoot(Nave, self.lastID)
+            self.shoots.append(NewShoot)
+            self.lastTime = CurrentTime
+            self.lastID += 1
+
+    def updateAll(self):
+        for shoot in self.shoots:
+            shoot.update()
+
+    def drawAll(self, screen):
+        for shoot in self.shoots:
+            shoot.draw(screen)
+
+    def deleteShoot(self, ID):
+        i = 0
+        for shoot in self.shoots:
+            if shoot.ID == ID:
+                index = i
+            i += 1
+
+        del self.shoots[index]
+
+
+ManagerShoots = ShootManage()
+
+
+class Shoot:
+    def __init__(self, Nave, ID):
+        self.direction = Nave.angle + m.pi
+        self.vel = ManagerShoots.ShootVeloc
+        self.x_pos = Nave.x_pos
+        self.y_pos = Nave.y_pos
+        self.ID = ID
+
+    def update(self):
+        self.x_pos += self.vel * m.sin(self.direction)
+        self.y_pos += self.vel * m.cos(self.direction)
+
+        Cond_a = self.x_pos < -2 * \
+            ManagerShoots.shootSize or self.x_pos > (
+                SCREEN_WIDTH + 2 * ManagerShoots.shootSize)
+        Cond_b = self.y_pos < -2 * \
+            ManagerShoots.shootSize or self.y_pos > (
+                SCREEN_HEIGHT + 2 * ManagerShoots.shootSize)
+
+        if Cond_a or Cond_b:
+            ManagerShoots.deleteShoot(ID=self.ID)
+
+    def draw(self, screen):
+        size = ManagerShoots.shootSize
+        pygame.draw.circle(screen, RED, (int(self.x_pos),
+                                         int(self.y_pos)), int(size))
 
 
 class SpaceShip:
@@ -204,6 +294,7 @@ def main():
         # Analisando teclas pressionadas
         keys = np.array(pygame.key.get_pressed())
         PressedKeys = np.where(keys == 1)[0]
+        # print(PressedKeys)
         for key in PressedKeys:
             if key == 119:
                 Nave.Acelerate("ver", "-")
@@ -217,6 +308,8 @@ def main():
                 Nave.Rotate("-")
             elif key == 276:
                 Nave.Rotate("+")
+            elif key == 273:
+                ManagerShoots.shoot(Nave)
 
         # --- Desenhando
         screen.fill(BLACK)
@@ -229,6 +322,10 @@ def main():
         ManagerAsters.RandomCreation()
         ManagerAsters.updateAll()
         ManagerAsters.drawAll(screen)
+
+        # Desenhando os tiros
+        ManagerShoots.updateAll()
+        ManagerShoots.drawAll(screen)
 
         # Definindo FPS
         clock.tick(60)
